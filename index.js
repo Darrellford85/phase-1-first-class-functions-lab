5495425c444c98fdfd6fb50110bf6a79180ebd1d
Darrell Ford <Darrellford85@gmail.com>
2022-03-26 09:57:25 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+/* Am I Crazy about Js or is it just coding?*/
+
+
+// First two only using .slice //
+const returnFirstTwoDrivers = function(driverslist) {
+  const fTwoD = driverslist.slice(0,2);
+      return fTwoD;
+}
+// Last two only using .slice//
+const returnLastTwoDrivers = function(driverslist) {
+    const lTwoD = driverslist.slice(-2);
+    return lTwoD;
+}
+// selectingDrivers fun//
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier()//
+function createFareMultiplier(cost){
+const multiplier = function(){
+    return cost * cost;
+    };
+    return multiplier
+}
+// fareDoubler()//
+function fareDoubler(cost) {
+    return (cost *2);
+    
+}
+//fareTripler()//
+function fareTripler(cost){
+    return (cost *3);
+}
+//  selectDifferentDrivers(arrayOfDrivers, function)//
+function selectDifferentDrivers(drivers, difFirstOrLast){
+return difFirstOrLast(drivers);
+}
